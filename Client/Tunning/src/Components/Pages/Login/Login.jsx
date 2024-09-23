00f1927c450f6af30e550dc42ba215c5ce96998c
andretini <andrealijaramosagostini@gmail.com>
2024-09-23 20:47:19 -0300

    Routing React

---
+++ Client/Tunning/src/Components/Pages/Login/Login.jsx
@@ -0,0 +1,8 @@
+export function LoginPage(){
+
+    return (
+        <div>
+            <p>Login</p>
+        </div>
+    )
+}
--- Client/Tunning/src/main.jsx
+++ Client/Tunning/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import { HomePage } from "./Components/Pages/Home/HomePage";
+import { LoginPage } from "./Components/Pages/Login/Login";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  }, 
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

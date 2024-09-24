9544d1a1ffad5b0fd8b1081c61d09cba109f6d30
andretini <andrealijaramosagostini@gmail.com>
2024-09-23 21:37:34 -0300

    Mysql connection

--- Server/Main.py
+++ Server/Main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
-import mysql
+import mysql.connector
+
+# Establish the connection
+connection = mysql.connector.connect(
+    host="localhost",         # Replace with your host
+    user="root",     # Replace with your username
+    password="1234", # Replace with your password
+    database="tunning"  # Replace with your database name
+)
+
+cursor = connection.cursor()
+
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/usuarios")
+async def getUsuarios():
+    cursor.execute("SELECT * FROM users")
+    return cursor.fetchall()
+
